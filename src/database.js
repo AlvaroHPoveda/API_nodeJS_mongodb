91ce6203fb787383f185e737fabd95e4e645926e
Alvaro Poveda <alvaropoveda@ingenieros.com>
2022-06-06 17:22:42 -0500

    update

--- src/database.js
+++ src/database.js
@@ -1,10 +1,16 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+import config from "./config";
 
-mongoose
-  .connect("mongodb://localhost/notes-db-app", {
-    // useCreateIndex: true,
-    useNewUrlParser: true,
-    // useFindAndModify: false,
-  })
-  .then((db) => console.log("DB is connected"))
-  .catch((err) => console.log(err));
+(async () => {
+  try {
+    const db = await mongoose.connect(config.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log("Mongodb is connected to", db.connection.host);
+  } catch (error) {
+    console.error(error);
+  }
+})();
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/notes/add", (req, res) => {
+  res.render("notes/new-note");
+});
+
+router.post('/notes/new-note', (req, res) => {
+  console.log(req.body); 
+  res.send('ok'); 
+})
+
 router.get("/notes", (req, res) => {
     res.send("Notes from database");
   });
